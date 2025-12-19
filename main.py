965fe2de90cbfd8e23a59359efe176ef54b4f4d4
Hesron Janaran H. Carvalho Pereira <janaran52@gmail.com>
2025-12-19 11:51:02 -0300

    v2

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   
+    nome1 = ""
+    nome2 = ""
+    data_inicio_iso = "2025-11-29T00:00:00" 
+    
+    return render_template('cont.html', n1=nome1, n2=nome2, data=data_inicio_iso)
+
+if __name__ == '__main__':
+ 
+    app.run(debug=True)
